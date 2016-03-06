c9cc851302e460529825c96c652aa51a075abcae
JeanMichelDube <jean.michel.dube@hotmail.fr>
2016-03-06 11:54:31 -0500

    Corrected Legend

--- plot3.R
+++ plot3.R
@@ -21,12 +21,12 @@ subdata$DateTime = paste(subdata$Date,subdata$Time)
 subdata$DateTime = strptime(subdata$DateTime,format = "%Y-%m-%d %H:%M:%S")
 
 ##Build Plot 3
+png("plot3.png",width=480,height=480,units="px")
 plot(subdata$DateTime,as.numeric(as.character(subdata$Sub_metering_1)),type = "l",xlab = "",ylab = "Energy sub metering")
 lines(subdata$DateTime,as.numeric(as.character(subdata$Sub_metering_2)), type="l",col="red")
 lines(subdata$DateTime,as.numeric(as.character(subdata$Sub_metering_3)), type="l",col="blue")
-legend( "topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), col= c("black", "red", "blue"))
+legend('topright',c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c('black','red','blue'), lty = 1, lwd = 3)
 
 ##Copy as png
-dev.copy(png, file = "plot3.png")
 dev.off()
 
